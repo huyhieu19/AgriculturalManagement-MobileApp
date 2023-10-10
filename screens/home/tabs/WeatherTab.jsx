770a74fb0058435df7212708882ddcf671d831f4
HuyHieu <huyhieu.hust2855@gmail.com>
2023-10-10 22:53:34 +0700

    Update

--- screens/home/tabs/WeatherTab.jsx
+++ screens/home/tabs/WeatherTab.jsx
@@ -1,11 +1,22 @@
 //import liraries
 import React from "react";
 import { View, Text, SafeAreaView } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import * as Progress from "react-native-progress";
 
 const WeatherTab = () => {
   return (
     <SafeAreaView>
-      <Text>WeatherTab</Text>
+      <StatusBar className="black" />
+      <View className="flex-1 relative"></View>
+      <View className="flex-1 flex-row justify-center items-center">
+        <Progress.CircleSnail
+          thickness={10}
+          size={140}
+          color="#0bb3b2"
+          className="bg-white"
+        />
+      </View>
     </SafeAreaView>
   );
 };
