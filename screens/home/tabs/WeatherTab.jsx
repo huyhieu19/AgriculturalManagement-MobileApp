9807c5bc52e9f4fee25a579bab1fa4e743dfdec7
HuyHieu <huyhieu.hust2855@gmail.com>
2023-10-10 21:49:46 +0700

    [DATN-FE]Change to weather page

---
+++ screens/home/tabs/WeatherTab.jsx
@@ -0,0 +1,13 @@
+//import liraries
+import React from "react";
+import { View, Text, SafeAreaView } from "react-native";
+
+const WeatherTab = () => {
+  return (
+    <SafeAreaView>
+      <Text>WeatherTab</Text>
+    </SafeAreaView>
+  );
+};
+
+export default WeatherTab;
