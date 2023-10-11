71cd02e7bb9ff5a5bb28630fd1fa7d4fece5e635
HuyHieu <huyhieu.hust2855@gmail.com>
2023-10-12 01:09:48 +0700

    [DATN-FE] Create home farm

--- assets/icons/index.js
+++ assets/icons/index.js
@@ -1,3 +1,10 @@
 export const IC_HOME = require("./ic_home.png");
 export const IC_SUN = require("./clear-sky.png");
 export const IC_PROFILE = require("./ic_profile.png");
+
+export const IC_SETTINGS = require("./ic_settings.png");
+export const IC_FARM = require("./ic_farm.png");
+export const IC_GROWTH = require("./ic_growth.png");
+export const IC_NOTIFICATION = require("./ic_notification.png");
+export const IC_TASK = require("./ic_to-do-list.png");
+export const IC_ANALYTICS = require("./ic_analytics.png");
