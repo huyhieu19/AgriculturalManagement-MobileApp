b7ecac323ca16e2287c675f4f0de1b43176813ff
HuyHieu <huyhieu.hust2855@gmail.com>
2024-02-20 00:37:31 +0700

    Add eas

--- network/configs/index.ts
+++ network/configs/index.ts
@@ -1,5 +1,9 @@
 export const AppConfig = {
-    //baseUrl: "https://agricultural-management.azurewebsites.net/api"
-    baseUrl: "https://localhost:7060/api"
+    baseUrl: "https://agricultural-management.azurewebsites.net/api"
+    //baseUrl: "https://localhost:7060/api"
     //baseUrl: "http://192.168.1.138:83/api"  //home
 }
+
+// Deployment
+// 1. eas build -p android 
+// 2. eas build -p android --profile preview
